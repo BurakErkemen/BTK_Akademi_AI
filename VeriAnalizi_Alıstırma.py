47f0cf9a952ed50bd97d3c8e047fdd5e8e1cd480
Burak Furkan ERKEMEN <burakfurkanerkemen@gmail.com>
2023-11-05 02:46:07 +0300

    Update

--- VeriAnalizi_Alıstırma.py
+++ VeriAnalizi_Alıstırma.py
@@ -41,6 +41,84 @@ yas[:,1:4] = imputer.transform(yas[:,1:4])
 # daha sonrasında transform fonk ile bu değerler dizinin istenilen row'una atılıyor
 
 # =============================================================================
-# KATEGORİK VERİLER
+# 2.6 - KATEGORİK VERİLER
+# **Kategorik veri örneği: Kadın mı? Erkek mi?
+# **Kategorik Veri --> 1. Nominal 2. Ordinal
+# *1.Nominal = Sıralama Yapılamaz ve ilişki kurulamaz. Cep telefonu markası gibi
+# *2.Ordinal = Sıralama yapılabilir örneğin şehir plaka kodları
+# **Sayısal Veri --> 1. Oransal 2. Aralık
+# *1.Oransal = birbirine çapılıp bölünebilen dört işleme müsait
+# *2.Aralık = Dört işleme müsait olmayan bir aralığın içindeymiş gibi olan verilerdir
+# **Kategorik veriler sayıya çevirilerek eğitilme işleminde kullanılırlar ve model 
+# eğitiminde rol oynarlar. 
 # =============================================================================
+ulke = veriler.iloc[:,0:1].values # Ulke column gelir
+from sklearn import preprocessing 
+le = preprocessing.LabelEncoder()
+#  sütunun değerlerini sayısal bir forma dönüştürmek için LabelEncoder kullanır
+ulke[:,0] = le.fit_transform(veriler.iloc[:,0])
+# print(ulke)
+
+ohe = preprocessing.OneHotEncoder()
+# Ardından, OneHotEncoder kullanılır. OneHotEncoder, kategorik verilerin sayısal 
+# dönüşümünü alır ve her kategori için ayrı bir sütun oluşturur. Her satır, ilgili 
+# kategoriye aitse, o sütun için 1 değerini alırken, diğer sütunlar için 0 değerini alır.
+ulke = ohe.fit_transform(ulke).toarray()
+# print(ulke)
+
+# =============================================================================
+# 2.7 - VERİLERİN BİRLEŞTİRİLMESİ ve DATAFRAME OLUSUMU
+# =============================================================================
+sonuc_ulke_df = pd.DataFrame(data=ulke,index = range(22), columns=['fr','tr','us'])
+# print(sonuc_ulke_df)
+sonuc_yas_df = pd.DataFrame(data=yas,index = range(22), columns=['boy','kilo','yas'])
+# print(sonuc_yas_df)
+
+cinsiyet = veriler.iloc[:,-1].values
+sonuc_cinsiyet_df=pd.DataFrame(data=cinsiyet,index=range(22),columns=['cinsiyet'])
+# print(sonuc_cinsiyet_df)
+
+# DATAFRAME BİRLEŞTİRME 
+s = pd.concat([sonuc_yas_df,sonuc_ulke_df],axis=1)
+# print(s)
+
+s_son = pd.concat([s,sonuc_cinsiyet_df],axis=1)
+print(s_son) # Bu çıktı ile datasetimiz güncellendi 
+
+# =============================================================================
+# 2.8 - VERİ KÜMESİNİN EĞİTİM ve TEST İÇİN BÖLÜNMESİ
+# =============================================================================
+
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(s
+                                                 ,sonuc_cinsiyet_df
+                                                 ,test_size=0.33
+                                                 ,random_state=0)
+# x değeri bağımsız değişkenler --> cinsiyet dışındakiler
+# y değeri bağıumlı değişkenler --> cinsiyet
+
+# =============================================================================
+# 2.9 - ÖZNİTELİK ÖLÇEKLEME
+# =============================================================================
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+
+X_Train = sc.fit_transform(x_train)
+X_Test = sc.fit_transform(x_test)
+
+
+# =============================================================================
+# 2.10 - VERİ ÖN İŞLEME ŞABLONU
+# =============================================================================
+
+
+
+
+
+
+
+
+
 
