58c687b00178b41aba34cfc5f6fd4203602d7f23
Burak Furkan ERKEMEN <burakfurkanerkemen@gmail.com>
2023-11-11 17:34:22 +0300

    Update

--- PolinomalRegresyon.py
+++ PolinomalRegresyon.py
@@ -4,9 +4,111 @@
 # =============================================================================
 
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
-
-
+# Veri Yükleme
 veriler = pd.read_csv('B:\GitHUB\BTK_Akademi_AI\Datasetler/maaslar.csv')
+
+# Data Frame Dilimleme 
+
+x = veriler.iloc[:,1:2]
+y= veriler.iloc[:,2:]
+
+# Tip Dönüşümü - Numpy Array'e dönüştürüldü
+X = x.values
+Y = y.values
+
+
+# Linear(Doğrusal) Model oluşturma ve görselleştirme 
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(X,Y)
+# =============================================================================
+# plt.scatter(x,y,color='red')
+# plt.plot(x,lr.predict(X),color='blue')
+# =============================================================================
+
+
+
+# Polynomial Regression (Dpğrusal olmayan) model oluşturma 
+from sklearn.preprocessing import PolynomialFeatures
+pr = PolynomialFeatures(degree=2) #2. derece polinom
+
+x_poly = pr.fit_transform(X)
+lr2 = LinearRegression()
+lr2.fit(x_poly,y)
+
+
+# 4. Dereceden polinom
+pr = PolynomialFeatures(degree=4)
+lr3 = LinearRegression()
+x_poly2 = pr.fit_transform(X)
+lr3.fit(x_poly2,y)
+
+# =============================================================================
+# Görselleştirme
+# =============================================================================
+plt.scatter(X, Y,color='red')
+plt.plot(X,lr2.predict(pr.fit_transform(X)),color='blue')
+plt.show()
+
+plt.scatter(X, Y,color='red')
+plt.plot(X,lr3.predict(pr.fit_transform(X)),color='blue')
+plt.show()
+
+
+# Tahminler
+print("Eğitim Seviyesi 11: ",lr.predict([[11]]))
+# Linear Reg için değeri= 34716
+print("Eğitim Seviyesi 6.6: ",lr.predict([[6.6]]))
+# Linear Reg için değeri = 16923
+
+print("Eğitim Seviyesi 11: ",lr2.predict(pr.fit_transform([[11]])))
+# Polynomial Reg için değeri = 89041
+print("Eğitim Seviyesi 6.6: ",lr2.predict(pr.fit_transform([[6.6]])))
+# Polynomial Reg için değeri = 8146 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SupportVectorRegression.py
@@ -0,0 +1,99 @@
+# Support Vector Regression 
+# =============================================================================
+# Bir doğru ile ayrılacak ve o doğruya iki paralel doğru ile arasındaki
+# maksimum değerleri alan en küçük marjine sahip doğruları bulmak amaçlanmıştır
+# Marjinal (Aşırı aykırı) verilere karşı zaafiyeti vardır
+# =============================================================================
+
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+# Veri yükleme
+veriler = pd.read_csv('B:\GitHUB\BTK_Akademi_AI\Datasetler/maaslar.csv')
+
+# Data Frame Dilimleme 
+
+x = veriler.iloc[:,1:2]
+y= veriler.iloc[:,2:]
+
+# Tip Dönüşümü - Numpy Array'e dönüştürüldü
+X = x.values
+Y = y.values
+
+# Veri Ölçeklendirme - Scaler yapmak zorundayız
+from sklearn.preprocessing import StandardScaler
+sc1 = StandardScaler()
+x_olcekli = sc1.fit_transform(X)
+
+sc2 = StandardScaler()
+y_olcekli = sc2.fit_transform(Y)
+
+# SVR Model oluşturma
+from sklearn.svm import SVR
+svr_reg = SVR(kernel='rbf') #kernel parametresi önemli
+svr_reg.fit(x_olcekli, y_olcekli)
+
+# =============================================================================
+# plt.scatter(x_olcekli, y_olcekli,color="red")
+# plt.plot(x_olcekli,svr_reg.predict(x_olcekli),color="blue")
+# =============================================================================
+# print(svr_reg.predict([[11]]))
+# print(svr_reg.predict([[6.6]]))
+
+# Tahmin Yapma 
+tahmin_girdisi = [[11]]
+tahmin_girdisi_olcekli = sc1.transform(tahmin_girdisi)
+tahmin_olcekli = svr_reg.predict(tahmin_girdisi_olcekli)
+tahmin_normal = sc2.inverse_transform(tahmin_olcekli.reshape(1, -1))
+
+print("Ölçekli Tahmin:", tahmin_olcekli)
+print("Normal Tahmin:", tahmin_normal)
+
+# =============================================================================
+# Çıktı
+# rbf yerine poly değeriyle verilen çıktı daha beklenilesi bir değerdedir.
+# Normal Tahmin: [[64028.60248]] 
+# rbf değeri
+# Normal Tahmin: [[27272.57392]]
+# =============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
