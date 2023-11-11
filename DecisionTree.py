8ced59c0b23026cff99e3ca327beaea0b32e1843
Burak Furkan ERKEMEN <burakfurkanerkemen@gmail.com>
2023-11-11 17:34:43 +0300

    Update

---
+++ DecisionTree.py
@@ -0,0 +1,67 @@
+# KARAR AĞAÇLARI İLE TAHMİN 
+# =============================================================================
+# DECİSİON TREE
+# İki boyutlu bir uzaya bölünür ve buna göre tahmin yapılır
+# Boydan veya kilodan bölünür ve sonrasında diğer değişkene bakılarak bölünür
+# 
+# 
+# =============================================================================
+
+
+
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+# Veri yükleme
+veriler = pd.read_csv('B:\GitHUB\BTK_Akademi_AI\Datasetler/veriler.csv')
+
+# =============================================================================
+# boy ve kilo'ya göre yas tahmini yapmak
+# =============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
